5c42549e546d3ed609922cc61cb023138aa50416
khalillovv <khalillovv.alexander@gmail.com>
2024-10-16 18:13:46 +0300

    fix cors

--- cafe-olena-backend/src/main.ts
+++ cafe-olena-backend/src/main.ts
@@ -17,6 +17,7 @@ async function bootstrap() {
 		origin: [CORS_ADDRESS],
 		methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 		credentials: true,
+		allowedHeaders: 'Content-Type, Authorization',
 		exposedHeaders: 'set-cookie',
 	})
 
