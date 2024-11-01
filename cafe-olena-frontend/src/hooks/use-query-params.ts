92f5b1b195e3fe40aae39aa2d440e19088ca889c
khalillovv <khalillovv.alexander@gmail.com>
2024-11-01 17:28:56 +0200

    fix menuId

--- cafe-olena-frontend/src/hooks/use-query-params.ts
+++ cafe-olena-frontend/src/hooks/use-query-params.ts
@@ -1,10 +1,16 @@
 import { useSearchParams } from 'next/navigation'
+import { useMemo } from 'react'
 import { useMenuData } from './use-menu-data'
 
 export const useQueryParams = () => {
 	const { filteredMenu } = useMenuData()
 	const searchParams = useSearchParams()
-	const menuId = Number(searchParams.get('menuId')) || filteredMenu?.[0].id || 0
+	const menuId = useMemo(() => {
+		if (searchParams.get('menuId')) {
+			return Number(searchParams.get('menuId'))
+		}
+		return filteredMenu && filteredMenu.length > 0 ? filteredMenu[0].id : 0
+	}, [searchParams, filteredMenu])
 	const categoryName = searchParams.get('cat')
 	const searchValue = searchParams.get('value')
 
